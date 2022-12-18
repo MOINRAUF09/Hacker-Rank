910c5bd1145b4d4cbcc7e80d7c52b5f7dbf44136
MoinRauf <moinrauf05@gmail.com>
2022-12-18 23:34:03 +0500

    first upload

---
+++ function.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+//CHANGES
+void return_greater_number(){
+  //int large;
+    int a[4];
+    //int i=0;
+    for (int i=0;i<4;i++){
+       cin>>a[i];
+    }
+    //for (int i=0;i<4;i++){
+       // cout<<arr[i];
+   // }
+  int temp;
+   int i,j=0;
+  for(i = 0; i<4; i++) {
+   for(j = i+1; j<4; j++)
+   {
+      if(a[j] < a[i]) {
+         temp = a[i];
+         a[i] = a[j];
+         a[j] =  temp;
+      }
+   }
+}
+  //for (int i=0;i<5;i++){
+    //    cout<<a[4];
+    //}
+    cout<<a[3];
+    //return large;
+}
+
+
+
+int main(){
+    return_greater_number();
+}
